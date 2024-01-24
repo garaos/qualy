2c8475a88e24fa4665310ebff8b388637173b815
garaos <g.araos@gmail.com>
2024-01-24 11:20:48 -0300

    estructura carga 2

--- src/contents/Home.jsx
+++ src/contents/Home.jsx
@@ -1,21 +1,22 @@
 import { Link } from 'react-router-dom'
+import Video from '../video/loop01.mp4'
 
 const Home = () => {
     return (
-        <div className='fondohome'>
-                <div className='col-12 col-md-9 col-lg-7 col-xl-6 pt-5 px-5'>
-                    <div classNme='container-fluid py-5 mt-5 px-3'>
-                        <h1 className='fw-bold text-white'>Colageno</h1>
-                        <p className='text-white shadou pt-2'>
+        <div className='fondohome '>
+            <div className='pt-5 px-5'>
+                <div classNme='container-fluid py-5 mt-5 px-3 '>
+                    <h1 className='fw-bold text-white'>Colageno</h1>
+                    <p className='text-white shadou pt-2'>
                         hola
-                        </p>
-                    </div>
-                    <div className='botoncitojt'>
-                        <Link to='/Producto'>
-                            <button className='btn btn-warning btn-lg mt-5 mb-5 fw-bold rounded-pill'>holi</button>
-                        </Link>
-                    </div>
+                    </p>
                 </div>
+                <div className='botoncitojt '>
+                    <Link to='/Producto'>
+                        <button className='btn btn-secondary btn-lg mt-5 mb-5 fw-bold rounded-pill'>Conoce Mas</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
