648ecb06ab7d8f553d81915c16e627b71c42a1fc
garaos <g.araos@gmail.com>
2024-01-25 15:33:57 -0300

    video responsivo

--- src/contents/Home.jsx
+++ src/contents/Home.jsx
@@ -3,22 +3,44 @@ import Video from '../video/loop01.mp4'
 
 const Home = () => {
     return (
-        <div className='fondohome '>
-            <div className='pt-5 px-5'>
-                <div classNme='container-fluid py-5 mt-5 px-3 '>
-                    <h1 className='fw-bold text-white'>Colageno</h1>
-                    <p className='text-white shadou pt-2'>
-                        hola
-                    </p>
-                </div>
-                <div className='botoncitojt '>
+<header id="header">
+    <div className="intro">
+        <video className="videointro" autoPlay loop muted>
+            <source src={Video} type='video/mp4' /></video>
+
+        <div className="overlay">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2 intro-text">
+                    <div className='botoncitojt '>
                     <Link to='/Producto'>
                         <button className='btn btn-secondary btn-lg mt-5 mb-5 fw-bold rounded-pill'>Conoce Mas</button>
                     </Link>
                 </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+</header>
+
+        // <div className='video-div1' id='video-text'>
+        //     <div className="intro">
+        //         <video className="videointro" autoPlay loop muted>
+        //             <source src={Video} type='video/mp4' /></video>
+        //     </div>
+
+
+        //     <div className='pt-5 px-5'>
+        //         <div className='botoncitojt '>
+        //             <Link to='/Producto'>
+        //                 <button className='btn btn-secondary btn-lg mt-5 mb-5 fw-bold rounded-pill'>Conoce Mas</button>
+        //             </Link>
+        //         </div>
+        //     </div>
+        // </div>
     );
 }
 
 export default Home;
+
