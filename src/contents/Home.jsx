9430f01faa0d8fac5281aab48b97d185ba05e014
garaos <g.araos@gmail.com>
2024-01-26 17:02:20 -0300

    boton transparante

--- src/contents/Home.jsx
+++ src/contents/Home.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 import Video from '../video/loop01.mp4'
 
 const Home = () => {
     return (
         <div className='main'>
-            <div className="overlay"></div>
             <video src={Video} autoPlay loop muted />
             <div className="content">
                 <Link to='/Producto'>
-                    <button className='btn btn-secondary btn-lg mt-5 mb-5 fw-bold rounded-pill'>Conoce Mas</button>
+                    <Button variant="outline-light" className='mt-5 mb-5 fw-bold rounded-pill'>Conoce Más</Button>
                 </Link>
             </div>
         </div>
