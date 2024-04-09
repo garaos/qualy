1ae85f281ee798d62c4caba012c62e5845b1a3c1
garaos <g.araos@gmail.com>
2024-04-09 15:02:31 -0400

    fuente

--- src/contents/Colageno.jsx
+++ src/contents/Colageno.jsx
@@ -3,6 +3,7 @@ import img2 from '../img/colag1.jpg'
 import img3 from '../img/colag2.jpg'
 import img4 from '../img/colag3.jpg'
 import img5 from '../img/colag4.jpg'
+import { Card } from 'react-bootstrap'
 
 const Colageno = () => {
     return (
@@ -11,7 +12,7 @@ const Colageno = () => {
                 <div className="px-5 pt-5 bg-collagen">
                     <div className="pt-5 px-5 pb-5 row">
                         <div className='col-sm-6 pt-5 px-5'>
-                            <img src={img1} alt="Colageno 1" width="100%" />
+                            <img src={img1} alt="Colageno 1" width="100%" className='shadow rounded'/>
                         </div>
                         <div className='col-sm-6 pt-1'>
                             <h1>ACERCA DEL COLAGENO QUALY VIE</h1><br />
@@ -58,64 +59,64 @@ const Colageno = () => {
                         <p>Los principals beneficios del consumo regular de colágeno son:</p><br />
 
                         <div className="row">
-                            <div className="col-sm-6 mb-3 mb-sm-0">
-                                <div className="card">
-                                    <img src={img2} alt="Colageno" width="100%" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">BELLEZA:</h5>
-                                        <p className="card-text"><ul>
+
+                            <div className="col-sm-6 pt-3">
+                                <Card className='h-100 shadow'>
+                                    <Card.Img src={img2} alt="Colageno" width="100%" />
+                                    <Card.Body>
+                                        <Card.Title>BELLEZA:</Card.Title>
+                                        <Card.Text><ul>
                                             <li>Estimula al cuerpo a producir más colágeno</li>
                                             <li>Piel más elástica e hidratada</li>
                                             <li>Aumento de los efectos antioxidantes que ayudan a mantener la piel más saludable</li>
                                             <li>Ayuda al crecimiento del pelo y uñas</li>
-                                        </ul></p>
-                                    </div>
-                                </div>
+                                        </ul></Card.Text>
+                                    </Card.Body>
+                                </Card>
                             </div>
-                            <div className="col-sm-6">
-                                <div className="card">
-                                    <img src={img3} alt="Colageno" width="100%" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">ARTICULACIONES:</h5>
-                                        <p className="card-text">   <ul>
+                            <div className="col-sm-6 pt-3">
+                                <Card className='h-100 shadow'>
+                                    <Card.Img src={img3} alt="Colageno" width="100%" />
+                                    <Card.Body>
+                                        <Card.Title>ARTICULACIONES:</Card.Title>
+                                        <Card.Text><ul>
                                             <li>Ayuda a reducir problemas degenerativos en cartílagos</li>
                                             <li>Reduce los dolores incomodos</li>
                                             <li>Mantener articulaciones más saludables</li>
                                             <li>Mejora el desempeño de las articulaciones</li>
-                                        </ul></p>
-                                    </div>
-                                </div>
+                                        </ul></Card.Text>
+                                    </Card.Body>
+                                </Card>
                             </div>
-                        </div>
-                        <div className="row mt-5">
-                            <div className="col-sm-6 mb-3 mb-sm-0">
-                                <div className="card">
-                                    <img src={img4} alt="Colageno" width="100%" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">HUESOS:</h5>
-                                        <p className="card-text">    <ul>
+                            <div className="col-sm-6 pt-3">
+                                <Card className='h-100 shadow'>
+                                    <Card.Img src={img4} alt="Colageno" width="100%" />
+                                    <Card.Body>
+                                        <Card.Title>HUESOS:</Card.Title>
+                                        <Card.Text><ul>
                                             <li>Ayuda a la salud y el fortalecimiento óseo</li>
                                             <li>Ayuda en la reducción de la pérdida ósea</li>
                                             <li>Mejora la densidad y composición mineral del hueso</li>
-                                        </ul></p>
-                                    </div>
-                                </div>
+                                        </ul></Card.Text>
+                                    </Card.Body>
+                                </Card>
                             </div>
-                            <div className="col-sm-6">
-                                <div className="card">
-                                    <img src={img5} alt="Colageno" width="100%" />
-                                    <div className="card-body">
-                                        <h5 className="card-title">SALUD:</h5>
-                                        <p className="card-text">  <ul>
+                            <div className="col-sm-6 pt-3">
+                                <Card className='h-100 shadow'>
+                                    <Card.Img src={img5} alt="Colageno" width="100%" />
+                                    <Card.Body>
+                                        <Card.Title>SALUD:</Card.Title>
+                                        <Card.Text><ul>
                                             <li>Fuente de proteínas</li>
                                             <li>Mantener la masa magra en el cuerpo</li>
                                             <li>Mantener la sensación de saciedad</li>
                                             <li>Ayuda a la curación de heridas</li>
-                                        </ul></p>
-                                    </div>
-                                </div>
+                                        </ul></Card.Text>
+                                    </Card.Body>
+                                </Card>
                             </div>
                         </div>
+
                     </div>
 
                 </div>
@@ -125,14 +126,14 @@ const Colageno = () => {
 
                 <div className='mt-5 bg-collagen'>
                     <div className='mx-5 mb-5 mt-5'>
-                    <div className="pt-5 px-5 pb-5 ">
-                        <h1>COMO CONSUMIR EL COLAGENO QUALY VIE</h1><br />
-                        <p className="justificado">Para conseguir resultados en nuestra salud gracias al consume de colágeno tenemos que hacer del consume de colágeno un hábito ya que, nuestro cuerpo está permanentemente perdiendo colágeno por la exposición a la radiación UV del sol, por el envejecimiento, por los cambios hormonales (menopausia), por la mala alimentación y déficit nutricional especialmente de Vitamina C, Zinc y cobre, por el estilo de vida poco sano (cigarro y alcohol). Por lo tanto, para combatir estos factores cotidianos debemos hacer del consume de colágeno algo cotidiano.<br />
-                            La mejor forma de consumir colágeno es disolver 2 cucharadas de té (10 gr) en medio vaso de agua (100 ml) y tomarlo en ayunas. Se puede tomar con agua a de la llave a temperatura ambiente o mezclarlo en el té o café de la mañana. También se puede mezclar con lecho o yogurt. Mezclarlo con bebidas muy heladas no tienen ninguna contraindicación salvo que será un poco más difícil disolver el colágeno..<br />
-                            Tomar una segunda dosis de 10 gr junto con la última comida del día también se recomienda, pero para la mayoría de las personas una dosis diaria de 10 gr debería ser más que suficiente.<br />
-                            De acuerdo a estudios, los resultados visibles del consume de colágeno se van a empezar a ver a partir del tercer mes de consumo regular.
-                        </p>
-                    </div>
+                        <div className="pt-5 px-5 pb-5 ">
+                            <h1>COMO CONSUMIR EL COLAGENO QUALY VIE</h1><br />
+                            <p className="justificado">Para conseguir resultados en nuestra salud gracias al consume de colágeno tenemos que hacer del consume de colágeno un hábito ya que, nuestro cuerpo está permanentemente perdiendo colágeno por la exposición a la radiación UV del sol, por el envejecimiento, por los cambios hormonales (menopausia), por la mala alimentación y déficit nutricional especialmente de Vitamina C, Zinc y cobre, por el estilo de vida poco sano (cigarro y alcohol). Por lo tanto, para combatir estos factores cotidianos debemos hacer del consume de colágeno algo cotidiano.<br />
+                                La mejor forma de consumir colágeno es disolver 2 cucharadas de té (10 gr) en medio vaso de agua (100 ml) y tomarlo en ayunas. Se puede tomar con agua a de la llave a temperatura ambiente o mezclarlo en el té o café de la mañana. También se puede mezclar con lecho o yogurt. Mezclarlo con bebidas muy heladas no tienen ninguna contraindicación salvo que será un poco más difícil disolver el colágeno..<br />
+                                Tomar una segunda dosis de 10 gr junto con la última comida del día también se recomienda, pero para la mayoría de las personas una dosis diaria de 10 gr debería ser más que suficiente.<br />
+                                De acuerdo a estudios, los resultados visibles del consume de colágeno se van a empezar a ver a partir del tercer mes de consumo regular.
+                            </p>
+                        </div>
                     </div>
                 </div>
 
